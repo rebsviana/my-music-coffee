0fcf7843e883ad20193b383095dbe2822858cbbc
rebsviana <rebecav@ciandt.com>
2022-08-22 17:26:42 -0300

    Test GItHub Actions

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -11,6 +11,8 @@ public class MusicController {
 
     @GetMapping
     public ResponseEntity<String> get() {
+        System.out.println("Teste");
         return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
     }
+
 }
