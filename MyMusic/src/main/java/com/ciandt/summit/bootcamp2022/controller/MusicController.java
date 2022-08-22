440eb502aa4cd9e91bf35ed190c309009fe69104
Rebeca Baptista <110989810+rebsviana@users.noreply.github.com>
2022-08-22 19:41:41 -0300

    Test GitHub Action

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -11,8 +11,6 @@ public class MusicController {
 
     @GetMapping
     public ResponseEntity<String> get() {
-        System.out.println("Test");
         return ResponseEntity.ok("67f5976c-eb1e-404e-8220-2c2a8a23be47");
     }
-
 }
