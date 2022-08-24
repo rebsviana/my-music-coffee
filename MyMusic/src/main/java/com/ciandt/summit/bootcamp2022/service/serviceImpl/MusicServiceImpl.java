c881f54f03ff5c99c94f4519f6abee219515734c
rebsviana <rebecav@ciandt.com>
2022-08-24 17:08:46 -0300

    BOOT-941/Implement exceptions treatment service

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/MusicServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/MusicServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ciandt.summit.bootcamp2022.service.serviceImpl;
 
 import com.ciandt.summit.bootcamp2022.dto.MusicDto;
+import com.ciandt.summit.bootcamp2022.exceptions.MinLengthRequiredException;
+import com.ciandt.summit.bootcamp2022.exceptions.NoContentException;
 import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
 import com.ciandt.summit.bootcamp2022.service.MusicService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +20,14 @@ public class MusicServiceImpl implements MusicService {
 
     @Override
     public List<MusicDto> searchMusicNameOrArtistName(String name) {
+        if (name.length() < 3)
+            throw new MinLengthRequiredException();
+
         var musicEntity = musicRepository.searchMusicNameOrArtistName(name);
+
+        if (musicEntity.isEmpty())
+            throw new NoContentException();
+
         return musicEntity.stream()
                 .map(music -> mapper.convertValue(music, MusicDto.class))
                 .collect(Collectors.toList());
--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/MusicServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/MusicServiceImplTest.java
@@ -1,11 +1,14 @@
 package com.ciandt.summit.bootcamp2022.service.serviceImpl;
 
 import com.ciandt.summit.bootcamp2022.dto.MusicDto;
+import com.ciandt.summit.bootcamp2022.exceptions.MinLengthRequiredException;
+import com.ciandt.summit.bootcamp2022.exceptions.NoContentException;
 import com.ciandt.summit.bootcamp2022.model.Artist;
 import com.ciandt.summit.bootcamp2022.model.Music;
 import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,7 +19,10 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class MusicServiceImplTest {
@@ -40,7 +46,7 @@ class MusicServiceImplTest {
 
     @Test
     void whenSearchMusicNameOrArtistNameThenReturnListOfMusicDto() {
-        Mockito.when(musicRepository.searchMusicNameOrArtistName(Mockito.anyString()))
+        Mockito.when(musicRepository.searchMusicNameOrArtistName(anyString()))
                 .thenReturn(List.of(music));
 
         Mockito.when(mapper.convertValue(music, MusicDto.class))
@@ -52,5 +58,31 @@ class MusicServiceImplTest {
         assertNotEquals(list.get(0).getArtistId().getName(), containsString("Harley"));
 
         //TODO: Buscar metodo para comparar se há pelo menos um dos dois
+    }
+
+    @Test
+    @DisplayName("When the name has less than three characters then return MinLengthRequiredException")
+    void whenSearchMusicNameOrArtistNameThenReturnMinLengthRequiredException() {
+        var exception = assertThrows(MinLengthRequiredException.class,
+                () -> service.searchMusicNameOrArtistName("aa"));
+
+        assertNotNull(exception);
+        assertEquals(MinLengthRequiredException.MESSAGE, exception.getMessage());
+        assertEquals(MinLengthRequiredException.class, exception.getClass());
+        verify(musicRepository, times(0)).searchMusicNameOrArtistName(anyString());
+    }
+
+    @Test
+    @DisplayName("When the list is empty then return NoContentException")
+    void whenSearchMusicNameOrArtistNameThenReturnNoContentException() {
+        Mockito.when(musicRepository.searchMusicNameOrArtistName(anyString()))
+                .thenReturn(List.of());
+
+        var exception = assertThrows(NoContentException.class,
+                () -> service.searchMusicNameOrArtistName("rebeca"));
+
+        assertNotNull(exception);
+        assertEquals(NoContentException.MESSAGE, exception.getMessage());
+        assertEquals(NoContentException.class, exception.getClass());
     }
 }
