737c81c2480fc31becf3e926d301f489d71f3519
rebsviana <rebecav@ciandt.com>
2022-08-24 17:07:38 -0300

    BOOT-941/Fix access modifiers in exceptions message

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exceptions/MinLengthRequiredException.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/exceptions/MinLengthRequiredException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MinLengthRequiredException extends RuntimeException{
 
-    private static final String MESSAGE = "The name must have three or more than three characters";
+    public static final String MESSAGE = "The name must have three or more than three characters";
 
     public MinLengthRequiredException(){
         super(MESSAGE);
