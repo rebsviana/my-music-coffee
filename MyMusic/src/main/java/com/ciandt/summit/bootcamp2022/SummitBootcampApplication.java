4e3a895220a85885fdbc283fec158439f6c355c3
rebsviana <rebecav@ciandt.com>
2022-08-26 08:57:25 -0300

    BOOT-884/Remove unused import

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/SummitBootcampApplication.java
@@ -5,7 +5,6 @@ import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages={
 		"com.ciandt.summit.bootcamp2022.model",
