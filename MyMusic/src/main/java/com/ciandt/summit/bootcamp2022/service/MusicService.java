720a3c1432a93c9b5c963fb6e57f360cb34900ea
luanadl <luanadl@ciandt.com>
2022-08-31 11:33:30 -0300

    BOOT-1061/Create getMusicById

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicService.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicService.java
@@ -5,4 +5,5 @@ import com.ciandt.summit.bootcamp2022.dto.MusicDto;
 import java.util.List;
 public interface MusicService {
     public List<MusicDto> getMusicByNameOrArtist(String name);
+    public  MusicDto getMusicById(String id);
 }
