7a8d1f2b5fa1a6d299f6892486bd60dfd78f8775
Claiver <claiver@ciandt.com>
2022-08-31 16:07:17 -0300

    BOOT-1058/Created PlaylistController

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/PlaylistController.java
@@ -0,0 +1,40 @@
+package com.ciandt.summit.bootcamp2022.controller;
+
+import com.ciandt.summit.bootcamp2022.dto.MusicDto;
+import com.ciandt.summit.bootcamp2022.service.serviceImpl.PlaylistServiceImpl;
+import com.ciandt.summit.bootcamp2022.service.serviceImpl.TokenAuthorizerService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/api/playlists", produces = "application/json")
+public class PlaylistController {
+
+    @Autowired
+    private PlaylistServiceImpl playlistService;
+    @Autowired
+    private TokenAuthorizerService tokenAuthorizerService;
+
+    @ApiOperation(value = "Save music in playlist", notes = "Saved new music in playlist")
+    @PostMapping("/{playlistId}/musicas")
+    public ResponseEntity<Void> saveMusicInPlaylist (@Valid @RequestBody MusicDto musicDto, @PathVariable("playlistId") String playlistId){
+        tokenAuthorizerService.verifyTokenAuthorizer();
+        var savedPlaylist = playlistService.saveMusicInPlaylist(musicDto, playlistId);
+
+        final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPlaylist.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+}
