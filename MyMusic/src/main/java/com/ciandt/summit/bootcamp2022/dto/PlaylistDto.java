ff05449eb7c81056e4c0d537fb94c5f6113c9e24
Claiver <claiver@ciandt.com>
2022-08-31 16:06:34 -0300

    BOOT-1058/Fix annotation, return type and table name

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/PlaylistDto.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/PlaylistDto.java
@@ -4,10 +4,13 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotNull;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class PlaylistDto {
 
