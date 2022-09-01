3c16dfc1c2cf74e1fa5fb1343a0b5ad3fe72bff5
Claiver <claiver@ciandt.com>
2022-09-01 16:08:02 -0300

    BOOT-1122/Remove unused dependencies

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/Music.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/Music.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/Playlist.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/Playlist.java
@@ -1,11 +1,8 @@
 package com.ciandt.summit.bootcamp2022.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
