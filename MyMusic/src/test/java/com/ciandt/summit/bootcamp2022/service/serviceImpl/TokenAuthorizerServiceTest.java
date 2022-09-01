51292560e2d61aa7e9488202154ac213cd5acb61
rebsviana <rebecav@ciandt.com>
2022-09-01 17:09:23 -0300

    BOOT-1123/Add import

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/TokenAuthorizerServiceTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/TokenAuthorizerServiceTest.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
