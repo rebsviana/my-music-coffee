f17a8077cb8b1979af892f5a440531edd0fbd82e
rebsviana <rebecav@ciandt.com>
2022-09-01 10:51:40 -0300

    BOOT-1117/Add token in header of request

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/MusicController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +26,10 @@ public class MusicController {
 
     @ApiOperation(value = "Get some music with filter", notes = "Returns a list of music")
     @GetMapping
-    public ResponseEntity<List<MusicDto>> getMusicByNameOrArtistWithFilter(@RequestParam("filtro") String filterName){
-        tokenAuthorizerService.verifyTokenAuthorizer();
+    public ResponseEntity<List<MusicDto>> getMusicByNameOrArtistWithFilter(@RequestParam("filtro") String filterName,
+                                                                           @RequestHeader(value="name") String name,
+                                                                           @RequestHeader(value="token") String token){
+        tokenAuthorizerService.verifyTokenAuthorizer(name, token);
         var listMusicDto = musicService.getMusicByNameOrArtist(filterName);
 
         return ResponseEntity.ok(listMusicDto);
