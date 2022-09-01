3d42c5d7ae51f88cd632300aaebc4f282ea1bb09
Claiver <claiver@ciandt.com>
2022-09-01 14:23:33 -0300

    BOOT-1045/TokenAuthorizer location change

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/token/CreateAuthorizerRequestData.java
@@ -0,0 +1,11 @@
+package com.ciandt.summit.bootcamp2022.model.token;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CreateAuthorizerRequestData {
+
+    private String name;
+
+    private String token;
+}
