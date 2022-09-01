4a1ee3fb0c220f6651369b6db71dfe954e6972bf
luanadl <luanadl@ciandt.com>
2022-09-01 17:01:16 -0300

    BOOT-1123/Remove all import using *

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/TokenAuthorizerServiceTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/TokenAuthorizerServiceTest.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
