eec7d21372a82866363ac9c693b281d1a50cbf54
Claiver <claiver@ciandt.com>
2022-09-02 09:45:15 -0300

    Fix token problems

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/PlaylistDto.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/dto/PlaylistDto.java
@@ -3,8 +3,10 @@ package com.ciandt.summit.bootcamp2022.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class PlaylistDto {
 
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/token/CreateAuthorizerRequest.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/token/CreateAuthorizerRequest.java
@@ -1,8 +1,10 @@
 package com.ciandt.summit.bootcamp2022.model.token;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class CreateAuthorizerRequest {
 
     private CreateAuthorizerRequestData data;
