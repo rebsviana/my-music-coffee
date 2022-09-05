75c7f1cee4230398225de3579a3d92d2283c1da7
peterls <peterleite.dev@gmail.com>
2022-09-05 14:01:43 -0300

    BOOT-1157/Implement delete mehod tests

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/PlaylistServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/PlaylistServiceImplTest.java
@@ -2,7 +2,9 @@ package com.ciandt.summit.bootcamp2022.service.serviceImpl;
 
 import com.ciandt.summit.bootcamp2022.dto.MusicDto;
 import com.ciandt.summit.bootcamp2022.dto.PlaylistDto;
+import com.ciandt.summit.bootcamp2022.exceptions.MusicDoesntExistException;
 import com.ciandt.summit.bootcamp2022.exceptions.PlaylistDoesntExistException;
+import com.ciandt.summit.bootcamp2022.model.Music;
 import com.ciandt.summit.bootcamp2022.model.Playlist;
 import com.ciandt.summit.bootcamp2022.repository.PlaylistsRepository;
 import com.ciandt.summit.bootcamp2022.tests.Factory;
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
+import static com.ciandt.summit.bootcamp2022.tests.Factory.MUSIC_ID;
 import static com.ciandt.summit.bootcamp2022.tests.Factory.PLAYLIST_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -40,6 +43,7 @@ class PlaylistServiceImplTest {
     private Playlist playlist;
     private PlaylistDto playlistDto;
     private MusicDto musicDto;
+    private Music music;
 
     public static final String ID_NOT_EXIST = "789456";
 
@@ -48,6 +52,7 @@ class PlaylistServiceImplTest {
         playlist = Factory.createPlaylist();
         playlistDto = Factory.createPlaylistDto();
         musicDto = Factory.createMusicDto();
+        music = Factory.createMusic();
     }
 
     @Test
@@ -108,6 +113,45 @@ class PlaylistServiceImplTest {
         verify(musicServiceImpl, times(1)).getMusicById(musicDto.getId());
         verify(playlistsRepository, times(1)).save(playlist);
         verify(mapper, times(2)).convertValue(playlist, PlaylistDto.class);
+        verify(mapper, times(1)).convertValue(playlistDto, Playlist.class);
+    }
+
+    @Test
+    @DisplayName("When delete music from playlist then remove it from playlist")
+    void whenDeleteMusicFromPlaylistThenRemoveItFromPlaylistThenUpdate() {
+        when(musicServiceImpl.getMusicById(anyString())).thenReturn(musicDto);
+        when(playlistsRepository.findById(anyString())).thenReturn(Optional.ofNullable(playlist));
+        when(playlistService.getPlaylistById(anyString())).thenReturn(playlistDto);
+        when(mapper.convertValue(playlistDto, Playlist.class)).thenReturn(playlist);
+        playlist.getMusics().add(music);
+        when(playlistsRepository.save(playlist)).thenReturn(playlist);
+
+        playlistService.deleteMusicFromPlaylist(MUSIC_ID, PLAYLIST_ID);
+
+        verify(musicServiceImpl, times(1)).getMusicById(musicDto.getId());
+        verify(playlistsRepository, times(1)).findById(playlist.getId());
+        verify(playlistsRepository, times(1)).save(playlist);
+        verify(mapper, times(1)).convertValue(playlistDto, Playlist.class);
+    }
+
+    @Test
+    @DisplayName("When delete music from playlist and music doesn't exist then return MusicDoesntExistException")
+    void whenDeleteMusicFromPlaylistAndMusicDoesntExistThenReturnMusicDoesntExistException() {
+        when(musicServiceImpl.getMusicById(anyString())).thenReturn(musicDto);
+        when(playlistsRepository.findById(anyString())).thenReturn(Optional.ofNullable(playlist));
+        when(playlistService.getPlaylistById(anyString())).thenReturn(playlistDto);
+        when(mapper.convertValue(playlistDto, Playlist.class)).thenReturn(playlist);
+
+        var exception = assertThrows(MusicDoesntExistException.class,
+                () -> playlistService.deleteMusicFromPlaylist(MUSIC_ID, PLAYLIST_ID));
+
+        assertNotNull(exception);
+        assertEquals(MusicDoesntExistException.class, exception.getClass());
+        assertEquals(MusicDoesntExistException.MESSAGE, exception.getMessage());
+
+        verify(musicServiceImpl, times(1)).getMusicById(musicDto.getId());
+        verify(playlistsRepository, times(1)).findById(playlist.getId());
+        verify(playlistsRepository, times(0)).save(playlist);
         verify(mapper, times(1)).convertValue(playlistDto, Playlist.class);
     }
 }
