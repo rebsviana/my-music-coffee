5bbe36e9167244118c4f6009f130ad66785e0a4c
Claiver <claiver@ciandt.com>
2022-09-09 14:46:06 -0300

    BOOT-1362/Create UserService and UserServiceImpl

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ciandt.summit.bootcamp2022.service.serviceImpl;
+
+import com.ciandt.summit.bootcamp2022.dto.PlaylistDto;
+import com.ciandt.summit.bootcamp2022.dto.UserDto;
+import com.ciandt.summit.bootcamp2022.model.Playlist;
+import com.ciandt.summit.bootcamp2022.model.User;
+import com.ciandt.summit.bootcamp2022.repository.PlaylistsRepository;
+import com.ciandt.summit.bootcamp2022.repository.UserRepository;
+import com.ciandt.summit.bootcamp2022.service.UserService;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class UserServiceImpl implements UserService {
+
+    private PlaylistsRepository playlistsRepository;
+    private UserRepository userRepository;
+
+    public void saveUser(UserDto userDto){
+        checkNotNull(userDto, "User is null");
+        checkNotNull(userDto.getName(), "Payload body incorrect: name is null");
+        checkNotNull(userDto.getNickname(), "Payload body incorrect: nickname is null");
+        checkNotNull(userDto.getUserType(), "Payload body incorrect: user type is null");
+
+        var playlist = playlistsRepository.save(new Playlist());
+
+        userDto.setPlaylistId(PlaylistDto.builder().id(playlist.getId()).build());
+
+        userRepository.save(
+                User.builder()
+                       .name(userDto.getName())
+                       .nickname(userDto.getNickname())
+                       .userType(userDto.getUserType())
+                       .playlistId(playlist)
+                       .build()
+        );
+    }
+}
