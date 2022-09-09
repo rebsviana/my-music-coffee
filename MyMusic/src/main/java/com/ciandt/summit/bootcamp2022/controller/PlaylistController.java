77075a13bcb38a1854bda9674902b9017f933d6c
rebsviana <rebecav@ciandt.com>
2022-09-09 16:13:26 -0300

    BOOT-1359/Change route in saveMusicInPlaylist

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/PlaylistController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/PlaylistController.java
@@ -38,15 +38,17 @@ public class PlaylistController {
             @ApiResponse(code = 400, message = Factory.MSG_400_MUSIC_DOESNT_EXIST + "<br/>" + Factory.MSG_400_PLAYLIST_DOESNT_EXIST),
             @ApiResponse(code = 500, message = Factory.MSG_500)
     })
-    @PostMapping("/{playlistId}/musicas")
+    @PostMapping("/{playlistId}/{userId}/music")
     public ResponseEntity<Void> saveMusicInPlaylist (@RequestBody MusicDto musicDto,
-                                                     @PathVariable("playlistId") String playlistId,
+                                                     @PathVariable String playlistId,
+                                                     @PathVariable String userId,
                                                      @RequestHeader(value="name") String userName,
                                                      @RequestHeader(value="token") String userToken){
         log.info("Starting the route save music in a playlist with id:" + playlistId);
         tokenAuthorizerService.verifyTokenAuthorizer(userName, userToken);
         log.info("Authorized user:" + userName);
-        var savedPlaylist = playlistService.saveMusicInPlaylist(musicDto, playlistId);
+
+        var savedPlaylist = playlistService.saveMusicInPlaylist(musicDto, playlistId, userId);
 
         final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
