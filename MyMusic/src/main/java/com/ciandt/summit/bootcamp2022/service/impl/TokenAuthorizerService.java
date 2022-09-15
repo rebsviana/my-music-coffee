056166fdecb624724030bce78739bcb7628acd7c
rebsviana <rebecav@ciandt.com>
2022-09-15 11:34:32 -0300

    BOOT-1426/Fix split in verifyTokenAuthorizer

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/impl/TokenAuthorizerService.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/impl/TokenAuthorizerService.java
@@ -23,8 +23,8 @@ public class TokenAuthorizerService {
             var authorizationEncode = requestAuthorization.substring(7);
             var authorizationDecode = Base64.getDecoder().decode(authorizationEncode);
             var authorization = new String(authorizationDecode);
-            var user = ":".split(authorization)[0];
-            var password = ":".split(authorization)[1];
+            var user = authorization.split(":")[0];
+            var password = authorization.split(":")[1];
 
             log.info("Starting integration with API Token Provider Service");
 
