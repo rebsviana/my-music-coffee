71a2867000e9ea6b5bb14837d8f254276ab98b48
peterls <peterls@ciandt.com>
2022-09-15 17:01:13 -0300

    Fix expection message

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/Factory.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/Factory.java
@@ -49,7 +49,7 @@ public class Factory {
     public static final String MSG_401_NO_RIGHTS = "You have not the rights to access this function!";
     public static final String MSG_200_USER_CREATED_SUCCESSFULLY = "User created successfully";
     public static final String MSG_400_USER_DOESNT_EXIST = "User doesn't exist";
-    public static final String MSG_400_PLAYLIST_DOESNT_EXIST_ON_USER = "Playlist doesn't exist";
+    public static final String MSG_400_PLAYLIST_DOESNT_EXIST_ON_USER = "Playlist doesn't exist on this user";
     public static final String MSG_400_NAME_HAVE_LESS_THAN_THREE_CHARACTERS = "The name must have three or more than three characters";
     public static final String MSG_400_MAX_MUSIC_CAPACITY_USER_COMMON = "Capacity of five musics in playlist reached for common user";
     public static final String MSG_400_MUSIC_ALREADY_EXIST_IN_THIS_PLAYLIST = "Music already exists in this playlist";
